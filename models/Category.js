a65087a76691bf2d3d5d1281787f4b6ac968a258
Steve Bour <bourpower@gmail.com>
2022-02-27 13:33:10 -0500

    models and dependencies written, sequelize added to server

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,20 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      notnull: true
+    }
   },
   {
     sequelize,
